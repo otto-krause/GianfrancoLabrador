33d2e35b675334c3be1f075465c2514832b92ce0
Gianfranco Labrador <141750443+Glashrke@users.noreply.github.com>
2023-08-28 22:48:01 -0300

    Update Ejercicio_5.c
    Descubrí que el código tenia un error, ya está corregido.

--- Ejercicio_5.c
+++ Ejercicio_5.c
@@ -2,38 +2,39 @@
 #include <stdlib.h>
 
 int sumaresistencias(int r1, int r2, int r3, int r4, int r5){
-	int r=0,rt,i=0;
-	
-	rt = r1+r2+r3+r4+r5;
+    int rt = r1 + r2 + r3 + r4 + r5;
+    return rt;
 }
-	return rt;
-	
-	
-	int main() {
-		int r,rt,r1,r2,r3,r4,r5,i,j;
-		for(i=1;i<=5;i++){
-			printf("Ingresa la resistencia\n");
-			scanf("%d",&r);
-			switch(i){
-			case 1:
-				r1 = r;
-				break;
-			case 2:
-				r2 = r;
-				break;
-			case 3:
-				r3 = r;
-				break;
-			case 4:
-				r4 = r;
-				break;
-			case 5:
-				r5 = r;
-				break; 
-			}
-		}
-		rt = sumaresistencias(r1,r2,r3,r4,r5);
-		
-		printf("La suma de las resistencias totales es: %d \n", rt);
-		printf("Las resistencias son: \n%d\n%d\n%d\n%d\n%d\n ",r1,r2,r3,r4,r5);
-	}
+
+int main() {
+    int r, rt, r1, r2, r3, r4, r5, i;
+    
+    for (i = 1; i <= 5; i++) {
+        printf("Ingresa la resistencia %d: ", i);
+        scanf("%d", &r);
+        switch (i) {
+            case 1:
+                r1 = r;
+                break;
+            case 2:
+                r2 = r;
+                break;
+            case 3:
+                r3 = r;
+                break;
+            case 4:
+                r4 = r;
+                break;
+            case 5:
+                r5 = r;
+                break; 
+        }
+    }
+    
+    rt = sumaresistencias(r1, r2, r3, r4, r5);
+    
+    printf("La suma de las resistencias totales es: %d \n", rt);
+    printf("Las resistencias son: \n%d\n%d\n%d\n%d\n%d\n", r1, r2, r3, r4, r5);
+    
+    return 0;
+}
